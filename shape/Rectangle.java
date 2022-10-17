aee871d69e261c4762d706e721c623f0b047aa30
AS <alwaled.alshammri@gmail.com>
2022-10-17 17:29:11 +0300

    edit The implementation of the getArea() method from

--- shape/Rectangle.java
+++ shape/Rectangle.java
@@ -9,16 +9,16 @@ public class Rectangle {
     private int length;
     private int breadth;
 
-    public Rectangle(int length, int breadth) {
-        this.length = length;
-        this.breadth = breadth;
-
+    public Rectangle(int l, int b) {
+        length = l;
+        breadth = b;
     }
+
     public int getArea() {
-        return length*breadth;  // write code to calculate area here
+        return length*breadth; // write code to calculate area here
     }
 
-    public int getperimeter() {
-        return (length+breadth)*2 ; }// write code to calculate perimeter here // getPerimeter()
+    // write code to calculate perimeter here
+    // getPerimeter()
 
 }
